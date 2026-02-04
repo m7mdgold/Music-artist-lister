775f27c5fe2b26c7c9a4d5e3d7ba17a25bf4d493
Golden <m7md0505431157@gmail.com>
2026-02-04 20:18:23 +0800

    Refactor Lister.py to allow user input for artist name and number of songs; update get_data function to accept song limit parameter.

--- Lister.py
+++ Lister.py
@@ -1,27 +1,35 @@
 import requests
+import cowsay
 import json
 
 
 def main():
-    artist = "michel jackson" #input("artist name: ")
-    data = get_data(artist)
+    artist = input("artist name: ")
+    num = input("how many songs you want to see ? :")
+    data = get_data(artist , num)
     lister(data)
 
 
 
 
-def get_data(artist):
-    data = requests.get("https://itunes.apple.com/search?entity=song&limit=2000&term=" + artist ).json()
+def get_data(artist , num):
+    data = requests.get("https://itunes.apple.com/search?entity=song&limit="+ num + "&term=" + artist ).json()
     
     return data
 
 
 def lister(data):
     names = data["results"]
+    i = 1
+    song = ""
     for track in names:
-        print(track["trackName"])
-    
+        if track["trackName"] not in song:
+            song += (f"{i}  {track["trackName"]} ") + "\n"
+            #
+            i += 1
 
+    
+    cowsay.cow(song)
 
 
 
